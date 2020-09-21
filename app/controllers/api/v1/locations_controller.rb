1cb00e9a60cb794a95951facaecd0cae4d058253
Amit Gupta <iiita.amitgupta@gmail.com>
2020-09-21 22:14:51 +0530

    API created, for access we need to visit: localhost:3000/api/v1/locations/1.json

---
+++ app/controllers/api/v1/locations_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::LocationsController < ApplicationController
+    before_action :set_location
+
+    def show
+    end
+
+    private
+
+        def set_location
+            @location = Location.find(params[:id])
+        end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :locations do
+        resources :recordings
+      end
+    end
+  end
+
+  resources :locations
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
