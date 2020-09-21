7ae29678718b6712b5454a2e227c01734437ae07
Amit Gupta <iiita.amitgupta@gmail.com>
2020-09-21 16:49:19 +0530

    Rails Application Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+l = Location.create(name: "New Delhi")
+l.recordings.create(temp: 32, status: "Cloudy")
+l.recordings.create(temp: 28, status: "Rainy")
+l.recordings.create(temp: 34, status: "Sunny")
+l.recordings.create(temp: 32, status: "Cloudy")
+l.recordings.create(temp: 30, status: "Rainy")
